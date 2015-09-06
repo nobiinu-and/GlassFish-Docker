ed4afdd0145933ce46c6e5cbc88fb9aaee5e97e7
Yuichiro Saito <saito@heartbeats.jp>
2015-09-06 10:59:47 +0900

    Add docker compose process.

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-sudo docker build -t team4/webapp .
+sudo docker build --rm -t koemu/webapp .
+sudo docker login --username=koemu --password=56805181 --email=netuser@mx.koemu.com
+sudo docker push koemu/webapp
