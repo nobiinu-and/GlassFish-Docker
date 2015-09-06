a73cb500344be3fd3d9314c498d8bf6f3698aa9c
Toshiyuki Ando <couger@kt.rim.or.jp>
2015-09-06 13:24:43 +0900

    dont movegit add .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export LATEST_ARTIFACT=$(find ~/myagent -name 'mrp.war' | grep drop | xargs ls -ltr | tail -1 | awk '{print $9}')
-mv $LATEST_ARTIFACT .
+cp -pf $LATEST_ARTIFACT .
 sudo docker build --rm -t koemu/webapp .
 sudo docker login --username=koemu --password=56805181 --email=netuser@mx.koemu.com
 sudo docker push koemu/webapp
