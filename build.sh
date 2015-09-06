78a912d7bce025ee9b7a36e6afba77348b035d61
Toshiyuki Ando <couger@kt.rim.or.jp>
2015-09-06 14:15:11 +0900

    fixed faild to copy artifact

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-$LATEST_ARTIFACT=/home/team4/myagent/_work/build/07b0c7f395008351ee66266376a333dbeac3851428d13a83f2cee4d54f076910/staging/drop/JavaEE7-App-On-Docker-1.0-SNAPSHOT.war
+LATEST_ARTIFACT=/home/team4/myagent/_work/build/07b0c7f395008351ee66266376a333dbeac3851428d13a83f2cee4d54f076910/staging/drop/JavaEE7-App-On-Docker-1.0-SNAPSHOT.war
 cp -pf $LATEST_ARTIFACT .
 
 sudo docker build --rm -t koemu/webapp .
